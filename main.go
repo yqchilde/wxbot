9a41c576f63b0c48c20cb41fc3b1bf7e22439603
yqchilde <yqchilde@gmail.com>
2022-08-30 18:08:04 +0800

    feat: 添加京东咚咚咚

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/yqchilde/wxbot/plugins/crazykfc"     // 肯德基疯狂星期四骚话
 	_ "github.com/yqchilde/wxbot/plugins/cronjob"      // 漂亮妹妹
 	_ "github.com/yqchilde/wxbot/plugins/emoticon"     // 表情包原图
+	_ "github.com/yqchilde/wxbot/plugins/jingdong"     // 京东咚咚咚
 	_ "github.com/yqchilde/wxbot/plugins/moyuban"      // 摸鱼办
 	_ "github.com/yqchilde/wxbot/plugins/pinyinsuoxie" // 拼音缩写翻译
 	_ "github.com/yqchilde/wxbot/plugins/plmm"         // 漂亮妹妹
---
+++ plugins/jingdong/main.go
@@ -0,0 +1,45 @@
+package jingdong
+
+import (
+	"os"
+	"strings"
+
+	"github.com/yqchilde/pkgs/log"
+
+	"github.com/yqchilde/wxbot/engine"
+	"github.com/yqchilde/wxbot/engine/robot"
+)
+
+type JingDong struct{ engine.PluginMagic }
+
+var (
+	pluginInfo = &JingDong{
+		engine.PluginMagic{
+			Desc:     "🚀 输入 /jd => 获取京东提交CK二维码",
+			Commands: []string{"/jd"},
+		},
+	}
+	_ = engine.InstallPlugin(pluginInfo)
+)
+
+func (p *JingDong) OnRegister() {}
+
+func (p *JingDong) OnEvent(msg *robot.Message) {
+	if msg != nil {
+		if msg.IsText() && msg.Content == "/jd" {
+			img, err := os.Open("./imgs/jingdong/qrcode.png")
+			if err != nil {
+				msg.ReplyText("Err: " + err.Error())
+			}
+			defer img.Close()
+
+			if _, err := msg.ReplyImage(img); err != nil {
+				if strings.Contains(err.Error(), "operate too often") {
+					msg.ReplyText("Warn: 被微信ban了，请稍后再试")
+				} else {
+					log.Errorf("msg.ReplyImage reply image error: %v", err)
+				}
+			}
+		}
+	}
+}
