83846441912c9fad6d9682759b90e359a80130b3
yqchilde <yqchilde@gmail.com>
2022-12-01 14:58:25 +0800

    refactor: 准备重构下

--- plugins/baidubaike/model.go
+++
@@ -1,8 +0,0 @@
-package baidubaike
-
-type ApiResponse struct {
-	Key        string `json:"key"`
-	Desc       string `json:"desc"`
-	Abstract   string `json:"abstract"`
-	Copyrights string `json:"copyrights"`
-}
