6f31e02faefff8bc9670b7fc8b5365472d732f77
yqchilde <yqchilde@gmail.com>
2022-12-30 15:37:13 +0800

    chore: 修改Dockerfile

--- build_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o wxbot
-mv ./wxbot ./docker/wxbot && cd ./docker || exit 0
-docker buildx build --platform=linux/amd64 -t yqchilde/wxbot .
-docker tag yqchilde/wxbot yqchilde/wxbot:latest
-docker push yqchilde/wxbot
-rm -f ./wxbot
