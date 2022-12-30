f0f7bcaba8bb79021dbf466071a7c5757b6734df
yqchilde <yqchilde@gmail.com>
2022-12-30 19:51:06 +0800

    chore: 修改一键运行docker脚本

--- docker/run.sh
+++ docker/run.sh
@@ -4,9 +4,10 @@ echo "1. 获取配置文件到config.yaml"
 wget -q -O config.yaml https://raw.fastgit.org/yqchilde/wxbot/hook/config.yaml
 chmod 755 config.yaml
 
-echo "2. 运行docker"
+echo "2. 检查wxbot容器"
+docker pull yqchilde/wxbot:latest
 docker ps | grep wxbot | awk '{print $1}' | xargs docker rm -f
+
+echo "3. 启动wxbot容器"
 docker run -dit --name wxbot -p 9528:9528 -v $(pwd)/config.yaml:/app/config.yaml -v $(pwd)/data:/app/data yqchilde/wxbot:latest
-printf "\033[A\33[2K"
-printf "\033[A\33[2K"
 echo "3. 启动完成，日志查看: docker logs -f wxbot"
