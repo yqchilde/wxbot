c55d27c15cc4dc4bbfadb7d3c3a963ce4d052902
yqchilde <yqchilde@gmail.com>
2023-02-06 21:23:57 +0800

    perf: chatgpt支持设置多个apiKey

--- plugins/chatgpt/README.md
+++ plugins/chatgpt/README.md
@@ -1,10 +1,10 @@
 ## 介绍
 
 * 插件名：ChatGPT聊天
-* 用法：发送`开始ChatGPT会话`即可进行连续会话，发送`结束ChatGPT会话`即可结束会话，或5分钟没新的提问自动结束
+* 用法：发送`开始会话`即可进行连续会话，发送`结束会话`即可结束会话，或5分钟没新的提问自动结束
 * 说明：需要获取apiKey，请到 [https://beta.openai.com](https://beta.openai.com) 获取
 * 其他命令：
-    * `set chatgpt apiKey __` 设置ChatGPT的apiKey
+    * `set chatgpt apiKey __(多个key用;符号隔开)` 设置ChatGPT的apiKey
     * `get chatgpt info` 获取ChatGPT的信息
 
 ## 预览：
--- plugins/chatgpt/main.go
+++ plugins/chatgpt/main.go
@@ -17,11 +17,16 @@ import (
 
 var (
 	db         sqlite.DB
-	chatGPT    ChatGPT
+	apiKeys    []ApiKey
 	gpt3Client gpt3.Client
 	chatCTXMap sync.Map // 群号/私聊:消息上下文
 	chatDone   = make(chan struct{})
 )
+
+// ApiKey api_key表，存放api_key
+type ApiKey struct {
+	Key string `gorm:"column:key;index"`
+}
 
 func init() {
 	engine := control.Register("chatgpt", &control.Options[*robot.Ctx]{
@@ -37,17 +42,21 @@ func init() {
 	if err := sqlite.Open(engine.GetDataFolder()+"/chatgpt.db", &db); err != nil {
 		log.Fatalf("open sqlite db failed: %v", err)
 	}
-	if err := db.CreateAndFirstOrCreate("chatgpt", &chatGPT); err != nil {
+	if err := db.Create("apikey", &ApiKey{}); err != nil {
 		log.Fatalf("create chatgpt table failed: %v", err)
 	}
 
-	gpt3Client = gpt3.NewClient(chatGPT.ApiKey, gpt3.WithTimeout(time.Minute))
-
-	engine.OnFullMatch("开始ChatGPT会话").SetBlock(true).Handle(func(ctx *robot.Ctx) {
-		if chatGPT.ApiKey == "" {
-			ctx.ReplyTextAndAt("请先私聊机器人配置apiKey\n指令：set chatgpt apiKey __\napiKey获取请到https://beta.openai.com获取")
+	engine.OnFullMatch("开始会话").SetBlock(true).Handle(func(ctx *robot.Ctx) {
+		if err := db.Orm.Table("apikey").Find(&apiKeys).Error; err != nil {
+			log.Errorf("开始ChatGPT会话失败，error:%s", err.Error())
+			ctx.ReplyTextAndAt("开启失败")
 			return
 		}
+		if len(apiKeys) == 0 {
+			ctx.ReplyTextAndAt("请先私聊机器人配置apiKey\n指令：set chatgpt apiKey __(多个key用;符号隔开)\napiKey获取请到https://beta.openai.com获取")
+			return
+		}
+		gpt3Client = gpt3.NewClient(apiKeys[0].Key, gpt3.WithTimeout(time.Minute))
 		if _, ok := chatCTXMap.Load(ctx.Event.FromUniqueID); ok {
 			ctx.ReplyTextAndAt("当前已经在进行ChatGPT会话了")
 			return
@@ -55,7 +64,7 @@ func init() {
 
 		recv, cancel := ctx.EventChannel(ctx.CheckGroupSession()).Repeat()
 		defer cancel()
-		ctx.ReplyTextAndAt("收到！已开始ChatGPT会话，输入\"结束ChatGPT会话\"结束会话，或5分钟后自动结束，请开始吧！")
+		ctx.ReplyTextAndAt("收到！已开始ChatGPT会话，输入\"开始会话\"结束会话，或5分钟后自动结束，请开始吧！")
 		chatCTXMap.LoadOrStore(ctx.Event.FromUniqueID, "")
 		for {
 			select {
@@ -71,7 +80,7 @@ func init() {
 				msg := c.MessageString()
 				if msg == "" {
 					continue
-				} else if msg == "结束ChatGPT会话" {
+				} else if msg == "结束会话" {
 					chatCTXMap.LoadAndDelete(ctx.Event.FromUniqueID)
 					ctx.ReplyText("已结束聊天的上下文语境，您可以重新发起提问")
 					return
@@ -107,28 +116,51 @@ func init() {
 
 	// 设置openai api key
 	engine.OnRegex("set chatgpt apiKey (.*)", robot.OnlyPrivate, robot.AdminPermission).SetBlock(true).Handle(func(ctx *robot.Ctx) {
-		apiKey := ctx.State["regex_matched"].([]string)[1]
-		if err := db.Orm.Table("chatgpt").Where("1 = 1").Update("api_key", apiKey).Error; err != nil {
+		var cacheApiKeys []string
+		if err := db.Orm.Table("apikey").Pluck("key", &cacheApiKeys).Error; err != nil {
+			log.Errorf("设置apiKey失败: %v", err)
 			ctx.ReplyTextAndAt("设置apiKey失败")
 			return
 		}
-		chatGPT.ApiKey = apiKey
-		gpt3Client = gpt3.NewClient(chatGPT.ApiKey, gpt3.WithTimeout(time.Minute))
+
+		matched := strings.Split(ctx.State["regex_matched"].([]string)[1], ";")
+		matchApiKeys := matched
+		for i := range cacheApiKeys {
+			for j := range matched {
+				if cacheApiKeys[i] == matched[j] {
+					matchApiKeys = append(matchApiKeys[:j], matchApiKeys[j+1:]...)
+				}
+			}
+		}
+
+		var apiKeys []ApiKey
+		for _, key := range matchApiKeys {
+			apiKeys = append(apiKeys, ApiKey{Key: key})
+		}
+		if err := db.Orm.Table("apikey").Create(&apiKeys).Error; err != nil {
+			ctx.ReplyTextAndAt("设置apiKey失败")
+			return
+		}
 		ctx.ReplyText("apiKey设置成功")
 	})
 
 	// 获取插件配置
 	engine.OnFullMatch("get chatgpt info", robot.OnlyPrivate, robot.AdminPermission).SetBlock(true).Handle(func(ctx *robot.Ctx) {
-		var chatGPT ChatGPT
-		if err := db.Orm.Table("chatgpt").Limit(1).Find(&chatGPT).Error; err != nil {
+		var apiKeys []ApiKey
+		if err := db.Orm.Table("apikey").Find(&apiKeys).Error; err != nil {
 			return
 		}
-		ctx.ReplyTextAndAt(fmt.Sprintf("插件 - ChatGPT\napiKey: %s", chatGPT.ApiKey))
+		if len(apiKeys) == 0 {
+			ctx.ReplyTextAndAt("插件 - ChatGPT\napiKey: 未设置")
+			return
+		}
+		var apiKeyMsg string
+		for i := range apiKeys {
+			log.Println(apiKeys[i])
+			apiKeyMsg += fmt.Sprintf("apiKey: %s\n", apiKeys[i].Key)
+		}
+		ctx.ReplyTextAndAt(fmt.Sprintf("插件 - ChatGPT\n%s", apiKeyMsg))
 	})
-}
-
-type ChatGPT struct {
-	ApiKey string `gorm:"column:api_key"`
 }
 
 func askChatGPT(question string) (string, error) {
@@ -142,7 +174,15 @@ func askChatGPT(question string) (string, error) {
 		FrequencyPenalty: 0,
 	})
 	if err != nil {
-		return "", err
+		if strings.Contains(err.Error(), "You exceeded your current quota") {
+			log.Printf("当前apiKey[%s]配额已用完, 将删除并切换到下一个", apiKeys[0].Key)
+			db.Orm.Table("apikey").Where("key = ?", apiKeys[0].Key).Delete(&ApiKey{})
+			apiKeys = apiKeys[1:]
+			gpt3Client = gpt3.NewClient(apiKeys[0].Key, gpt3.WithTimeout(time.Minute))
+			return askChatGPT(question)
+		} else {
+			return "", err
+		}
 	}
 	return resp.Choices[0].Text, nil
 }
