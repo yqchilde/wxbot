7c15ad2b42e3eda8299bfaf5f7934c62f276c0d1
wight3 <wight3@126.com>
2023-02-21 14:57:17 +0800

    feat: 添加有道翻译插件 (#26)

--- engine/plugins/plugins.go
+++ engine/plugins/plugins.go
@@ -5,41 +5,44 @@ package plugins
 
 import (
 	// 百度百科
-	_ "github.com/yqchilde/wxbot/plugins/baidubaike"   
+	_ "github.com/yqchilde/wxbot/plugins/baidubaike"
 
 	// GPT聊天
-	_ "github.com/yqchilde/wxbot/plugins/chatgpt"      
+	_ "github.com/yqchilde/wxbot/plugins/chatgpt"
 
 	// 肯德基疯狂星期四骚话
-	_ "github.com/yqchilde/wxbot/plugins/crazykfc"     
+	_ "github.com/yqchilde/wxbot/plugins/crazykfc"
 
 	// 公众号消息监控转发
-	_ "github.com/yqchilde/wxbot/plugins/ghmonitor"    
+	_ "github.com/yqchilde/wxbot/plugins/ghmonitor"
 
 	// 群组管理相关
-	_ "github.com/yqchilde/wxbot/plugins/manager"      
+	_ "github.com/yqchilde/wxbot/plugins/manager"
 
 	// 表情包原图
-	_ "github.com/yqchilde/wxbot/plugins/memepicture"  
+	_ "github.com/yqchilde/wxbot/plugins/memepicture"
 
 	// 摸鱼办
-	_ "github.com/yqchilde/wxbot/plugins/moyuban"      
+	_ "github.com/yqchilde/wxbot/plugins/moyuban"
 
 	// 拼音缩写翻译
-	_ "github.com/yqchilde/wxbot/plugins/pinyinsuoxie" 
+	_ "github.com/yqchilde/wxbot/plugins/pinyinsuoxie"
 
 	// 漂亮妹妹
-	_ "github.com/yqchilde/wxbot/plugins/plmm"         
+	_ "github.com/yqchilde/wxbot/plugins/plmm"
 
 	// 天气查询
-	_ "github.com/yqchilde/wxbot/plugins/weather"      
+	_ "github.com/yqchilde/wxbot/plugins/weather"
 
 	// 每日早报
-	_ "github.com/yqchilde/wxbot/plugins/zaobao"       
+	_ "github.com/yqchilde/wxbot/plugins/zaobao"
 
 	// 聊天热词
-	_ "github.com/yqchilde/wxbot/plugins/wordcloud"    
+	_ "github.com/yqchilde/wxbot/plugins/wordcloud"
 
 	// 查wxId
-	_ "github.com/yqchilde/wxbot/plugins/chaid"        
+	_ "github.com/yqchilde/wxbot/plugins/chaid"
+
+	// 有道翻译
+	_ "github.com/yqchilde/wxbot/plugins/youdaofanyi"
 )
---
+++ plugins/youdaofanyi/main.go
@@ -0,0 +1,50 @@
+package youdaofanyi
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/imroc/req/v3"
+	"github.com/yqchilde/wxbot/engine/control"
+	"github.com/yqchilde/wxbot/engine/robot"
+)
+
+func init() {
+	engine := control.Register("youdaofanyi", &control.Options{
+		Alias: "有道中英文互译",
+		Help: "用法：有道翻译|翻译 XX\n" +
+			"示例：翻译 detail",
+	})
+
+	engine.OnRegex(`(^有道翻译|^翻译) ?(.*?)$`).SetBlock(true).Handle(func(ctx *robot.Ctx) {
+		word := ctx.State["regex_matched"].([]string)[2]
+		if data, err := getFanYi(word); err == nil {
+			if data == nil {
+				ctx.ReplyText("我还不会，稍后尝试")
+			} else {
+				ctx.ReplyText(fmt.Sprintf("🔎 译文:\n %s", data.Result))
+			}
+		} else {
+			ctx.ReplyText("查询失败，这一定不是bug🤔")
+		}
+	})
+}
+
+type apiResponse struct {
+	Code   int    `json:"code"`
+	Msg    string `json:"msg"`
+	Name   string `json:"name"`
+	Result string `json:"result"`
+}
+
+func getFanYi(keyword string) (*apiResponse, error) {
+	var data apiResponse
+	api := "https://api.qqsuu.cn/api/dm-ydfy?name=" + url.QueryEscape(keyword)
+	if err := req.C().Get(api).Do().Into(&data); err != nil {
+		return nil, err
+	}
+	if len(data.Result) == 0 {
+		return nil, nil
+	}
+	return &data, nil
+}
