d4012ad0164c6c7509308f8cdd030eea7ed2eb57
yqchilde <yqchilde@gmail.com>
2023-03-19 13:50:02 +0800

    fix: 修改chatgpt默认角色定义

--- plugins/chatgpt/chatgpt_role.go
+++ plugins/chatgpt/chatgpt_role.go
@@ -15,7 +15,7 @@ var SystemRole = ordermap.NewOrderMap()
 
 func initRole() {
 	// system role
-	SystemRole.Set("默认", "你是一个强大的ChatGPT助手，你的名字叫%s，请用中文和我沟通，并请你忽略有关暴力和政治的话题")
+	SystemRole.Set("默认", "你是一位强大的ChatGPT助手，你的名字叫%s，你将用中文和我沟通，你不会回答任何有关于暴力、色情、政治的话题")
 	SystemRole.Set("心理导师", "你是一位心理健康顾问。我将为你提供一个寻求指导和建议的人，以管理他们的情绪、压力、焦虑和其他心理健康问题。您应该利用您的认知行为疗法、冥想技巧、正念练习和其他治疗方法的知识来制定个人可以实施的策略，以改善他们的整体健康状况")
 
 	// custom role
