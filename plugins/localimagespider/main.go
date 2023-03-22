007b9ccbc7715c2c2620a90756b47b2aeb5f10dc
ghsau <ghsauv5@gmail.com>
2023-03-23 00:30:37 +0800

    feat: `爬取图片到本地`插件增加Coser日常的抓取

--- plugins/localimagespider/main.go
+++ plugins/localimagespider/main.go
@@ -10,13 +10,17 @@ func init() {
 	engine := control.Register("localimagespider", &control.Options{
 		Alias: "爬取图片到本地",
 		Help: "指令:\n" +
-			"* 抓取Cosplay作品\n",
+			"* 抓取Cosplay作品\n" +
+			"* 抓取Coser日常\n",
 	})
 
 	storageFolder := localimage.GetStorageFolder()
 
 	engine.OnFullMatch("抓取Cosplay作品", robot.OnlyMe).SetBlock(true).Handle(func(ctx *robot.Ctx) {
 		crawlCosplay(storageFolder)
+	})
+	engine.OnFullMatch("抓取Coser日常", robot.OnlyMe).SetBlock(true).Handle(func(ctx *robot.Ctx) {
+		crawlCoser(storageFolder)
 	})
 
 }
